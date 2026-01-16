45b99d6d621b4740ddd1ea365eade260f615c96c
Ivonne C. Barco <ivonne.barco@loganitsolutions.com>
2026-01-16 14:30:05 -0500

    Fundamentos de LangGraph

---
+++ src/simple.py
@@ -0,0 +1,28 @@
+from typing import TypedDict
+
+class State(TypedDict):
+    customer_name: str
+    my_age: int
+
+state: State = {}
+customer_name = state.get("customer_name", None)
+print(f"Customer name: {customer_name}")
+
+def node_1(state: State):
+    if state.get("customer_name") is None:
+        return {
+            "customer_name": "Alice"
+        }
+    return {
+        "my_age": 30
+    }
+
+from langgraph.graph import StateGraph, START, END
+
+builder = StateGraph(State)
+builder.add_node("node_1", node_1)
+
+builder.add_edge(START, "node_1")
+builder.add_edge("node_1", END)
+
+agent = builder.compile()
